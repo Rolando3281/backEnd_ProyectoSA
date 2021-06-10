a4bbc1ebe09dd1d463b5aff309178107e50d0d81
rolando3281 <rolando.marroquin@gmail.com>
2021-06-09 22:18:35 -0600

    agrega end points libros

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(3000, () => {
 */
 //Conexion a la base de datos MongoDB ATLAS
 
-var database, collection;
+var database, collectionClientes, collectionLibros, collectionGenerosLibros;
 
 app.listen(3000, () => {
     console.log("Listening on 3000...");
@@ -46,18 +46,22 @@ app.listen(3000, () => {
             throw error;
         }
         database = client.db(DATABASE_NAME);
-        collection = database.collection("clientes");
+        collectionClientes = database.collection("clientes");
+        collectionLibros = database.collection("libros");
+        collectionGenerosLibros = database.collection("generosLibro");
         console.log("Connected to `" + DATABASE_NAME + "`!");
     });
 
 });
+ 
 
+/***********************************************************MANEJO DE USUARIOS/CLIENTES************************************************************/
 //LISTADO DE CLIENTES 
 app.get("/cliente",(request,response)=>{
 
     console.log("get request done!");
 
-    collection.find({}).toArray((error, result) =>{
+    collectionClientes.find({}).toArray((error, result) =>{
 
         if(error){
             return response.status(500).send(error);
@@ -74,7 +78,7 @@ app.get("/cliente/:id",(request,response)=>{
 
     console.log("get request done!");
 
-    collection.findOne({ "_id": new ObjectId(request.params.id) },(error, result) =>{
+    collectionClientes.findOne({ "_id": new ObjectId(request.params.id) },(error, result) =>{
 
         if(error){
             return response.status(500).send(error);
@@ -91,7 +95,7 @@ app.get("/porAprobar",(request,response)=>{
 
     console.log("get request done!");
 
-    collection.find({"aprobado":false}).toArray((error, result) =>{
+    collectionClientes.find({"aprobado":false}).toArray((error, result) =>{
 
         if(error){
             return response.status(500).send(error);
@@ -108,7 +112,7 @@ app.post("/cliente", (request, response) => {
 
     console.log("post request done!");
 
-    collection.insertOne(request.body, (error, result) => {
+    collectionClientes.insertOne(request.body, (error, result) => {
         if(error) {
             return response.status(500).send(error);
         }
@@ -118,10 +122,10 @@ app.post("/cliente", (request, response) => {
 });
 
 //APRUEBA UN USUARIO
-app.put("/cliente/:id",(request,response)=>{
+app.put("/aprobarClinte/:id",(request,response)=>{
 
     
-    collection.updateOne({"_id": new ObjectId(request.params.id)},{$set:{"aprobado":true}} ,(error, result) =>{
+    collectionClientes.updateOne({"_id": new ObjectId(request.params.id)},{$set:{"aprobado":true}} ,(error, result) =>{
 
         if(error){
             return response.status(500).send(error);
@@ -134,13 +138,158 @@ app.put("/cliente/:id",(request,response)=>{
 
 });
 
+//ACTUALIZA UN USUARIO
+app.put("/cliente/:id",(request,response)=>{
+
+    
+    collectionClientes.updateOne({"_id": new ObjectId(request.params.id)},
+                                {$set:{"nombres":request.body.nombres,
+                                       "apellidos":request.body.apellidos,
+                                        "email":request.body.email,
+                                        "password":request.body.password,
+                                        "direccion":request.body.direccion,
+                                        "celuar":request.body.celuar,
+                                        "tipo":request.body.tipo,
+                                        "aprobado":request.body.aprobado}} ,
+                                (error, result) =>{
+
+        if(error){
+            return response.status(500).send(error);
+        }
+
+        response.send(result);
+        console.log("PUT request done!");
+
+    });
+
+});
+
+
 //ELIMINA UN CLIENTE
 
 app.delete("/cliente/:id", (request, response) => {
 
     //console.log("delete request done!");
 
-    collection.deleteOne({"_id": new ObjectID(request.params.id)}, (error, result) => {
+    collectionClientes.deleteOne({"_id": new ObjectID(request.params.id)}, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+        console.log("delete request done!");
+    });
+});
+
+
+/***********************************************************MANEJO DE LIBROS************************************************************/
+
+//LISTADO DE LIBROS 
+app.get("/libros",(request,response)=>{
+
+    console.log("get request done!");
+
+    collectionLibros.find({}).toArray((error, result) =>{
+
+        if(error){
+            return response.status(500).send(error);
+        }
+
+        response.send(result);
+
+    });
+
+});
+
+
+//LISTA UN LIBRO POR ID
+app.get("/libros/:id",(request,response)=>{
+
+    
+    collectionLibros.findOne({ "_id": new ObjectId(request.params.id) },(error, result) =>{
+
+        if(error){
+            return response.status(500).send(error);
+        }
+
+        console.log("get request done!");
+        response.send(result);
+
+    });
+
+});
+
+//GUARDA UN LIBRO
+app.post("/libros", (request, response) => {
+   
+    collectionLibros.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.ops);
+        console.log("post request done!");
+    });
+});
+
+
+//OBTIENE GENEROS DE UN LIBRO
+
+app.get("/generosLibro/:id",(request,response)=>{
+
+    console.log("get request done!");
+
+    collectionGenerosLibros.find({"idLibro":request.params.id}).toArray((error, result) =>{
+
+        if(error){
+            return response.status(500).send(error);
+        }
+
+        response.send(result);
+
+    });
+
+});
+
+//AGREGA UN GENERO A UN LIBRO
+app.post("/generoLibro", (request, response) => {
+   
+    collectionGenerosLibros.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.ops);
+        console.log("post request done!");
+    });
+});
+
+//ACTUALIZA UN LIBRO
+app.put("/libros/:id",(request,response)=>{
+
+    
+    collectionLibros.updateOne({"_id": new ObjectId(request.params.id)},
+                                {$set:{"titulo":request.body.titulo,
+                                        "editorial":request.body.editorial,
+                                        "stock":  request.body.stock,
+                                        "imagen": request.body.imagen}} 
+                                ,(error, result) =>{
+
+        if(error){
+            return response.status(500).send(error);
+        }
+
+        response.send(result);
+        console.log("PUT request done!");
+
+    });
+
+});
+
+//ELIMINA UN LIBRO
+
+app.delete("/libros/:id", (request, response) => {
+
+    //console.log("delete request done!");
+
+    collectionLibros.deleteOne({"_id": new ObjectID(request.params.id)}, (error, result) => {
         if(error) {
             return response.status(500).send(error);
         }
